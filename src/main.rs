2d44fa8e8f96486b882616ec1ed47d378cca86de
Eduardo Oliveira <eduardooliveira256@gmail.com>
2022-10-12 18:54:29 -0300

    changeVariableType

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,31 @@ use std::io::{Write, ErrorKind, BufRead, BufReader};
 use std::fs::File;
 use std::cmp::Ordering;
 
-fn main() {
+fn main()
+{
+    changeVariableType()
+}
+
+fn nameQuestion()
+{
     println!("What is your name? ");
     let mut name = String::new();
     let greeting = "Nice to meet you!";
     io::stdin().read_line(&mut name)
-        .expect("Didn't receive input");
+    .expect("Didn't receive input");
     
     println!("Hello {}!, {}", name.trim_end(), greeting);
+}
+
+fn changeVariableType()
+{
+    const ONE_MILLION: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age = "19";
+    let mut age: u32 = age.trim().parse()
+    .expect("Age wasn't assigned a number!");
+
+    age = age + 1;
+
+    println!("I'm {} and I have ${}", age, ONE_MILLION);
 }
