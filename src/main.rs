1dabe83c20824b95cc6ac769ccc2ddb626dae50b
Eduardo Oliveira <eduardooliveira256@gmail.com>
2022-10-11 21:23:36 -0300

    What is your name?

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(unused)]
+#![allow(non_snake_case)]
 
 use std::io;
 use rand::Rng;
@@ -7,5 +8,11 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name? ");
+    let mut name = String::new();
+    let greeting = "Nice to meet you!";
+    io::stdin().read_line(&mut name)
+        .expect("Didn't receive input");
+    
+    println!("Hello {}!, {}", name.trim_end(), greeting);
 }
