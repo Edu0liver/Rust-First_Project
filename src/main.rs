6c270b54edca1d205c49aceac0c404f3de3c3815
Eduardo Oliveira <eduardooliveira256@gmail.com>
2022-10-17 09:42:23 -0300

    traits

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::ops::Add;
 use std::collections::HashMap;
 
 fn main() {
-    hashMaps();
+    traits();
 }
 
 fn nameQuestion() {
@@ -364,5 +364,48 @@ fn structs() {
         length: 4,
         height: 10.5
     };
+
+}
+
+fn traits() {
+    const PI: f32 = 3.141592;
+
+    trait Shape {
+        fn new(length: f32, width: f32) -> Self;
+        fn area(&self) -> f32;
+    }
+
+    struct Rectangle {
+        length: f32,
+        width: f32
+    }
+    struct Circle {
+        length: f32,
+        width: f32
+    }
+
+    impl Shape for Rectangle {
+        fn new(length: f32, width: f32) -> Rectangle {
+            return Rectangle{ length, width }
+        }
+        fn area(&self) -> f32 {
+            return self.length * self.width;
+        }
+    };
     
+    impl Shape for Circle {
+        fn new(length: f32, width: f32) -> Circle {
+            return Circle{ length, width }
+        }
+        fn area(&self) -> f32 {
+            return (self.length / 2.0).powf(2.0) * PI;
+        }
+    };
+
+    let rec: Rectangle = Shape::new(10.0, 10.0);
+    let circ: Circle = Shape::new(10.0, 10.0);
+
+    println!("Rectangle Area: {}", rec.area());
+    println!("Circle Area: {}", circ.area());
+
 }
