67f12848f2c3b52c1884a46563718a61b4158011
Eduardo Oliveira <eduardooliveira256@gmail.com>
2022-10-17 16:57:21 -0300

    Concurrency/Thread

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,17 @@ use std::fs::File;
 use std::cmp::Ordering;
 use std::ops::Add;
 use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::sync::{Arc, Mutex};
 
 mod restaurant;
 use crate::restaurant::orderFood;
 
 fn main() {
-    closures();
+    concurrencyThread();
 }
 
 fn nameQuestion() {
@@ -233,7 +238,7 @@ fn stringsOverview() {
 
 }
 
-fn casting(){
+fn casting() {
     let int_u8: u8 = 5;
     let int2_u8: u8 = 4;
     let int3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
@@ -241,7 +246,7 @@ fn casting(){
     println!("int3: {}", int3_u32);
 }
 
-fn enums(){
+fn enums() {
     enum Day {
         Monday,
         Tuesday,
@@ -509,5 +514,44 @@ fn boxConcept() {
     let node1 = TreeNode::new(1)
     .left(TreeNode::new(2))
     .right(TreeNode::new(3));
+
+}
+
+fn concurrencyThread() {
+    pub struct Bank {
+        balance: f32
+    }
+
+    fn withdraw(theBank: &Arc<Mutex<Bank>>, amt: f32) {
+        let mut bankRef = theBank.lock().unwrap();
+        
+        if bankRef.balance <= 5.00 {
+            println!("Current Balance: {} Withdraw a smaller amount!", bankRef.balance);
+        } else {
+            println!("Customer withdrew {}!\nCurrent Balance: {}", amt, bankRef.balance);
+            return bankRef.balance -= amt;
+        }
+    }
+
+    fn customer(theBank: Arc<Mutex<Bank>>) {
+        withdraw(&theBank, 5.00);
+    }
+
+    let bank: Arc<Mutex<Bank>> = Arc::new(Mutex::new(Bank {
+        balance: 20.00
+    }));
+
+    let handles = (0..10).map(|_| {
+        let bankRef = bank.clone();
+        thread::spawn(|| {
+            customer(bankRef)
+        })
+    });
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Total: {}", bank.lock().unwrap().balance);
 
 }
